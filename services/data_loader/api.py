b2b13df58589bcf522b3f7fd41494b150914a4fc
solhope23 <s526413103@gmail.com>
2025-08-20 13:08:25 +0300

    delete  - route

--- services/data_loader/api.py
+++ services/data_loader/api.py
@@ -39,6 +39,17 @@ class MyAPI:
                 return {"message": e, "status": "error"}
 
 
+        @self.app.delete("/delete")
+        def delete_doc(id_doc : bm_object.DeleteSoldier):
+            try:
+                self.dal.update(id_doc.__dict__)
+                return {"message": f"deleted soldier id - {id_doc.soldier_ID}", "status" : "succeeded"}
+            except Exception as e:
+                return {"message": e, "status": "error"}
+
+
+
+
 
 
 
