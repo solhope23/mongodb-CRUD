ffaf7386027e08d1d338b504c9e3887db8142b34
solhope23 <s526413103@gmail.com>
2025-08-20 13:06:28 +0300

    insert - route

--- services/data_loader/api.py
+++ services/data_loader/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import base_model_objects as bm_object
 
 class MyAPI:
 
@@ -18,6 +19,17 @@ class MyAPI:
         @self.app.get("/get_all")
         def get_all():
             return {"all_docs" : self.dal.read_all()}
+
+
+        @self.app.post("/insert")
+        def add_doc(doc : bm_object.Soldier):
+            try:
+                self.dal.write(doc.__dict__)
+                return {"message": f"inserted soldier {doc.first_name}, {doc.last_name}, with id {doc.soldier_ID} into the collection {self.dal.col}", "status" : "succeeded"}
+            except Exception as e:
+                return {"message": e, "status" : "error"}
+
+
 
 
 
